e3345ff73fd779d3303195adfea0147b40f33e1d
yuchting <yuchting@gmail.com>
2020-07-02 15:35:19 +0800

    add sample codes

---
+++ TrojanPlusApp/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using TrojanPlusApp.Models;
+
+namespace TrojanPlusApp.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
