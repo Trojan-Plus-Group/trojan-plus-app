e245ffa49d17e5b6589ccc4347635f8db67f4cbe
yuchting <yuchting@gmail.com>
2020-07-14 21:14:31 +0800

    add license comments

--- TrojanPlusApp.UWP/MainPage.xaml.cs
+++ TrojanPlusApp.UWP/MainPage.xaml.cs
@@ -1,17 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.UI.Xaml;
-using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
+﻿/*
+ * This file is part of the Trojan Plus project.
+ * Trojan is an unidentifiable mechanism that helps you bypass GFW.
+ * Trojan Plus is derived from original trojan project and writing
+ * for more experimental features.
+ * Copyright (C) 2020 The Trojan Plus Group Authors.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+using System;
 
 namespace TrojanPlusApp.UWP
 {
