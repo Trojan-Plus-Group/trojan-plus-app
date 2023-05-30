aae5f758aa3d6abd6631f981247fce74f2771456
yuchting <yuchting@gmail.com>
2023-05-30 17:11:09 +0800

    update gfwlist and cn ip list and Android version

--- TrojanPlusApp/Resources/update.sh
+++ TrojanPlusApp/Resources/update.sh
@@ -1,2 +1,4 @@
-curl https://cokebar.github.io/gfwlist2dnsmasq/gfwlist_domain.txt -o gfwlist.txt
+curl https://raw.githubusercontent.com/cokebar/gfwlist2dnsmasq/master/gfwlist2dnsmasq.sh -o gfwlist2dnsmasq.sh
+chmod +x gfwlist2dnsmasq.sh
+./gfwlist2dnsmasq.sh -l -o gfwlist.txt
 curl https://raw.githubusercontent.com/17mon/china_ip_list/master/china_ip_list.txt -o cn_mainland_ips.txt
