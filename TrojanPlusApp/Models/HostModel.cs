f8ac3ccb0b2e7ef9ec22ae86c3fde89caa2028be
Jerry_Tian <tianzhizhi.tzz@bytedance.com>
2026-01-25 16:16:19 +0800

    up: ssl option

--- TrojanPlusApp/Models/HostModel.cs
+++ TrojanPlusApp/Models/HostModel.cs
@@ -159,8 +159,8 @@ namespace TrojanPlusApp.Models
         "        \"verify\": ${ssl.verify},\n" +
         "        \"verify_hostname\": ${ssl.verify_hostname},\n" +
         "        \"cert\": \"${ssl.cert}\",\n" +
-        "        \"cipher\": \"ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-CHACHA20-POLY1305:ECDHE-RSA-CHACHA20-POLY1305:ECDHE-ECDSA-AES256-GCM-SHA384:ECDHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-AES256-SHA:ECDHE-ECDSA-AES128-SHA:ECDHE-RSA-AES128-SHA:ECDHE-RSA-AES256-SHA:DHE-RSA-AES128-SHA:DHE-RSA-AES256-SHA:AES128-SHA:AES256-SHA:DES-CBC3-SHA\",\n" +
-        "        \"cipher_tls13\": \"TLS_AES_128_GCM_SHA256:TLS_CHACHA20_POLY1305_SHA256:TLS_AES_256_GCM_SHA384\",\n" +
+        "        \"cipher\": \"ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-CHACHA20-POLY1305:ECDHE-RSA-CHACHA20-POLY1305:ECDHE-ECDSA-AES256-GCM-SHA384:ECDHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-AES256-SHA:ECDHE-RSA-AES256-SHA:DHE-RSA-AES256-SHA:AES256-SHA\",\n" +
+        "        \"cipher_tls13\": \"TLS_AES_256_GCM_SHA384:TLS_AES_128_GCM_SHA256:TLS_CHACHA20_POLY1305_SHA256\",\n" +
         "        \"sni\": \"${ssl.sni}\",\n" +
         "        \"alpn\": [\n" +
         "            \"h2\",\n" +
@@ -168,8 +168,8 @@ namespace TrojanPlusApp.Models
         "        ],\n" +
         "        \"reuse_session\": true,\n" +
         "        \"ssl_shutdown_wait_time\": ${ssl.ssl_shutdown_wait_time},\n" +
-        "        \"session_ticket\": false,\n" +
-        "        \"curves\": \"\"\n" +
+        "        \"session_ticket\": true,\n" +
+        "        \"curves\": \"X25519\"\n" +
         "    },\n" +
         "    \"tcp\": {\n" +
         "        \"no_delay\": true,\n" +
