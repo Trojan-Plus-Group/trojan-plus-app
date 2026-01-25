ac0cf7fbc869538b5ca7b782fdc52cc2d6084e20
Jerry_Tian <tianzhizhi.tzz@bytedance.com>
2026-01-25 08:04:44 +0800

    fix: enhance dark theme support and auto-switching across platforms

--- TrojanPlusApp.iOS/AppDelegate.cs
+++ TrojanPlusApp.iOS/AppDelegate.cs
@@ -1,4 +1,4 @@
-﻿/*
+/*
  * This file is part of the Trojan Plus project.
  * Trojan is an unidentifiable mechanism that helps you bypass GFW.
  * Trojan Plus is derived from original trojan project and writing
@@ -26,13 +26,17 @@ namespace TrojanPlusApp.iOS
     using Foundation;
     using TrojanPlusApp.Models;
     using UIKit;
+    using Microsoft.Maui;
+    using Microsoft.Maui.Hosting;
 
     // The UIApplicationDelegate for the application. This class is responsible for launching the 
     // User Interface of the application, as well as listening (and optionally responding) to 
     // application events from iOS.
     [Register("AppDelegate")]
-    public partial class AppDelegate : global::Xamarin.Forms.Platform.iOS.FormsApplicationDelegate
+    public partial class AppDelegate : global::Microsoft.Maui.MauiUIApplicationDelegate
     {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+
         public class Stater : TrojanPlusApp.App.IStart
         {
             public string GetTrojanPlusLibVersion()
@@ -55,21 +59,6 @@ namespace TrojanPlusApp.iOS
                 throw new NotImplementedException();
             }
 
-        }
-
-        //
-        // This method is invoked when the application has loaded and is ready to run. In this 
-        // method you should instantiate the window, load the UI into it and then make the window
-        // visible.
-        //
-        // You have 17 seconds to return from this method, or iOS will terminate your application.
-        //
-        public override bool FinishedLaunching(UIApplication app, NSDictionary options)
-        {
-            global::Xamarin.Forms.Forms.Init();
-            LoadApplication(new App("", new Stater()));
-
-            return base.FinishedLaunching(app, options);
         }
     }
 }
